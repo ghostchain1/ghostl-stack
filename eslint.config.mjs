526a8b0523eb12bfab145e34af90adcc4c40fbf2
ghostmode25 <ghosttoken.dev@gmail.com>
2026-01-12 22:58:59 -0500

    updatedb

---
+++ eslint.config.mjs
@@ -0,0 +1,52 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import nextPlugin from '@next/eslint-plugin-next';
+import prettier from 'eslint-config-prettier';
+
+export default tseslint.config(
+  {
+    ignores: [
+      '**/node_modules/**',
+      'dist/**',
+      'out/**',
+      'apps/web/.next/**',
+      'cache/**',
+      'chains/**/data/**',
+      'infra/opstack/**/data/**'
+    ]
+  },
+  {
+    files: ['**/*.{ts,tsx,js,jsx}'],
+    languageOptions: {
+      parserOptions: {
+        ecmaVersion: 2022,
+        sourceType: 'module'
+      },
+      globals: {
+        ...globals.node,
+        ...globals.browser
+      }
+    },
+    plugins: {
+      '@next/next': nextPlugin
+    },
+    settings: {
+      next: {
+        rootDir: ['apps/web']
+      }
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...tseslint.configs.recommended.rules,
+      ...nextPlugin.configs.recommended.rules,
+      ...(prettier.rules || {}),
+      'next/no-html-link-for-pages': 'off',
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        { argsIgnorePattern: '^_', varsIgnorePattern: '^_', caughtErrorsIgnorePattern: '^_' }
+      ]
+    }
+  }
+);
