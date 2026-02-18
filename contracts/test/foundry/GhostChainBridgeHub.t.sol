c44fbc1818d9274988c8780f09ef19b51daf063d
ghostmode25 <ghosttoken.dev@gmail.com>
2026-02-18 11:38:41 +0000

    bridge-hub: enforce l3 roots link to recorded l2 parent

--- contracts/test/foundry/GhostChainBridgeHub.t.sol
+++ contracts/test/foundry/GhostChainBridgeHub.t.sol
@@ -10,6 +10,7 @@ contract GhostChainBridgeHubTest is TestBase {
     address private constant OPERATOR = address(0x1111);
     uint8 private constant LAYER_L1 = 1;
     uint8 private constant LAYER_L2 = 2;
+    uint8 private constant LAYER_L3 = 3;
 
     function testLayerRootRecordingAndOutboundConstraint() public {
         GhostChainBridgeHub hub = new GhostChainBridgeHub(GOVERNOR, TIMELOCK, InterchainAuthorization(address(0)));
@@ -41,5 +42,37 @@ contract GhostChainBridgeHubTest is TestBase {
         vm.prank(OPERATOR);
         vm.expectRevert(abi.encodeWithSelector(GhostChainBridgeHub.OnlyGhostChainEgress.selector, uint8(2)));
         hub.queueOutboundMessage(uint8(2), 42161, address(0xAAAA), 100, keccak256("payload-2"));
+    }
+
+    function testL3RootRequiresParentL2Root() public {
+        GhostChainBridgeHub hub = new GhostChainBridgeHub(GOVERNOR, TIMELOCK, InterchainAuthorization(address(0)));
+
+        vm.prank(GOVERNOR);
+        hub.setOperator(OPERATOR, true);
+        vm.prank(GOVERNOR);
+        hub.setLayerRootPostingEnabled(LAYER_L2, true);
+        vm.prank(GOVERNOR);
+        hub.setLayerRootPostingEnabled(LAYER_L3, true);
+
+        bytes32 l2Root = keccak256("l2-root-parent");
+        bytes32 l3Root = keccak256("l3-root-child");
+
+        vm.prank(OPERATOR);
+        vm.expectRevert(abi.encodeWithSelector(GhostChainBridgeHub.L3RequiresParentL2Root.selector));
+        hub.recordLayerRoot(LAYER_L3, l3Root, 200, keccak256("l3-evidence-direct"));
+
+        vm.prank(OPERATOR);
+        vm.expectRevert(abi.encodeWithSelector(GhostChainBridgeHub.L2ParentRootNotRecorded.selector, l2Root));
+        hub.recordL3LayerRoot(l3Root, l2Root, 201, keccak256("l3-evidence-missing-parent"));
+
+        vm.prank(OPERATOR);
+        hub.recordLayerRoot(LAYER_L2, l2Root, 123, keccak256("l2-evidence"));
+
+        vm.prank(OPERATOR);
+        hub.recordL3LayerRoot(l3Root, l2Root, 202, keccak256("l3-evidence-linked"));
+
+        assertTrue(hub.hasLayerRoot(LAYER_L3, l3Root), "l3 root recorded");
+        assertEq(hub.l3ParentL2Roots(l3Root), l2Root, "l3 parent linked");
+        assertTrue(hub.isLinkedL3ToL2(l3Root, l2Root), "link verification");
     }
 }
